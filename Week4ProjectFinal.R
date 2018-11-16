a30e057fb3ff8022f6f0deb8b340f02da098c2b1
TravisJones01 <41902827+TravisJones01@users.noreply.github.com>
2018-11-16 10:55:01 -0500

    Update Week4ProjectFinal.R

--- Week4ProjectFinal.R
+++ Week4ProjectFinal.R
@@ -1,4 +1,16 @@
-#Load files (do not need the intertial signals files)
+#Merges the training and the test sets to create one data set.
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+#Uses descriptive activity names to name the activities in the data set
+#Appropriately labels the data set with descriptive variable names.
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+#Download and Load files (do not need the intertial signals files) as dataframe
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="auto")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+#moved unzipped folder to desktop for this part and forward
 activityLabels     <- read.table("C:/Users/travis.jones01/Desktop/UCI HAR Dataset/activity_labels.txt")
 dataFeaturesNames  <- read.table("C:/Users/travis.jones01/Desktop/UCI HAR Dataset/features.txt")
 
@@ -10,7 +22,7 @@ dataActivityTrain  <- read.table("C:/Users/travis.jones01/Desktop/UCI HAR Datase
 dataFeaturesTrain  <- read.table("C:/Users/travis.jones01/Desktop/UCI HAR Dataset/train/X_train.txt")
 dataSubjectTrain   <- read.table("C:/Users/travis.jones01/Desktop/UCI HAR Dataset/train/subject_train.txt")
 
-#Union the relevant subject, activity, and features files
+#Merge the relevant subject, activity, and features files
 dataSubject  <- rbind(dataSubjectTrain, dataSubjectTest)
 dataActivity <- rbind(dataActivityTrain, dataActivityTest)
 dataFeatures <- rbind(dataFeaturesTrain, dataFeaturesTest)
@@ -24,7 +36,7 @@ names(dataFeatures) <- dataFeaturesNames$V2
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
 
-#identify the types of data we are interested in (mean and std)
+#identify the types of data we are interested in (mean and std but not meanfreq)
 subdataFeaturesNames <- dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
 
 #add subject & activity and then filter combined data to only relevant records
@@ -48,6 +60,3 @@ library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
-
-library(knitr)
-knit2html("codebook.Rmd");
